bd0f868c6c7a045ff8b59532e41359bef068747b
lanjewarakash <lanjewarakash72@gmail.com>
2023-01-14 21:03:18 +0530

    Check Entered a valid Email Id or Not

--- src/com/blz/UserRegistration.java
+++ src/com/blz/UserRegistration.java
@@ -34,4 +34,19 @@ public class UserRegistration {
             System.out.println(lName + " Is a InValid LastName");
         }
     }
+
+    public void checkEmailId() {
+        System.out.println("Enter a Email Id");
+        Scanner sc = new Scanner(System.in);
+        String eId = sc.nextLine();
+
+        Pattern p = Pattern.compile("[a-zA-Z0-9.-]*@[a-zA-Z]+[.][a-zA-Z]+");
+        Matcher m = p.matcher(eId);
+
+        if (m.matches()) {
+            System.out.println(eId + " Is a Valid EmailId");
+        } else {
+            System.out.println(eId + " Is a InValid EmailId");
+        }
+    }
 }
